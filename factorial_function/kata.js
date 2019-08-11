4933796154c679bbd5db0ca6547533bca5faf61a
dislersd <dislersd@gmail.com>
2019-08-10 19:01:20 -0700

    added recursive solution

--- factorial_function/kata.js
+++ factorial_function/kata.js
@@ -1,10 +1,16 @@
 // first pass
 
 const factorial = n => {
-    let x = n
-    while (x > 1) {
-        n = n * (x - 1)
-        x -= 1
-    } 
-    return n
-}
+  let x = n;
+  while (x > 1) {
+    n = n * (x - 1);
+    x -= 1;
+  }
+  return n;
+};
+
+// recursive (with ternary)
+
+const factorialRecursive = n => {
+  return n ? n * factorialRecursive(n - 1) : 1;
+};
