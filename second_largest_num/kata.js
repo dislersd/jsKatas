ac4861b68f4f992f844f770c9dd151c336e5e34b
dislersd <dislersd@gmail.com>
2019-08-18 21:11:01 -0700

    second largest num one liner

--- second_largest_num/kata.js
+++ second_largest_num/kata.js
@@ -7,3 +7,9 @@ function getSecondLargest (nums) {
 }
 
 console.log(getSecondLargest([1,4,13,5,7,200,10,4,200,7,5])) // => 13
+
+// one liner
+
+const getSecondLargest2 = (nums) => [...new Set(nums)].sort((a,b) => b - a)[1]
+
+console.log(getSecondLargest2([1,80,20,10,120,4,120])) // => 80
