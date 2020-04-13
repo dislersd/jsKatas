0df8aa68c8deb7f483c203f58a08e84033f7b299
dislersd <dislersd@gmail.com>
2020-04-12 21:51:11 -0700

    single number

--- happy_number/kata.js
+++ happy_number/kata.js
@@ -1,14 +1,14 @@
 const isHappy = (n) => {
   for (let i = 0; i < 10; i++) {
-    let num = n
-    let nArr = []
+    let num = n;
+    let nArr = [];
     while (num > 0) {
-      nArr.push(Math.pow(num % 10, 2))
-      num = Math.floor(num / 10)
+      nArr.push(Math.pow(num % 10, 2));
+      num = Math.floor(num / 10);
     }
 
     n = nArr.reduce((total, current) => total + current, 0);
     if (n === 1) return true;
   }
-  return false
-}
+  return false;
+};
