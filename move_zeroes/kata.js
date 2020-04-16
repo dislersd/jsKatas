a7ab9bb91f2e4b00078e14949b78f5a1aec96d85
dislersd <dislersd@gmail.com>
2020-04-15 20:14:42 -0700

    move zeroes

--- move_zeroes/kata.js
+++ move_zeroes/kata.js
@@ -26,16 +26,31 @@ const moveZeroesInPlace1 = function (nums) {
 };
 
 // moves zeros in place!
-function moveZeroesInPlace2 (nums) {
-  let sorted = false 
+function moveZeroesInPlace2(nums) {
+  let sorted = false;
   while (sorted === false) {
-    sorted = true
+    sorted = true;
     for (let i = 0; i < nums.length - 1; i++) {
       if (nums[i] === 0 && nums[i + 1] !== 0) {
-        [nums[i], nums[i + 1]] = [nums[i + 1], nums[i]]
-        sorted = false
+        [nums[i], nums[i + 1]] = [nums[i + 1], nums[i]];
+        sorted = false;
       }
     }
+  }
+  return nums;
+}
+
+
+function moveZeroes3 (nums) {
+  debugger
+  let len = nums.length;
+  for(let i = 0; i < len; i++) {
+      if (nums[i] === 0) {
+          nums.splice(i, 1);
+          nums.push(0);
+          len -= 1;
+          i--
+      }
   }
   return nums
 }
@@ -45,3 +60,5 @@ console.log(moveZeroes([0, 1, 0, 3, 12])); // => [1,3,12,0,0]
 console.log(moveZeroesInPlace1([0, 1, 0, 3, 12])); // => [3,1,12,0,0]
 
 console.log(moveZeroesInPlace2([0, 1, 0, 3, 12])); // => [1,3,12,0,0]
+
+console.log(moveZeroes3([0, 0, 1])); // => [1,3,12,0,0]
