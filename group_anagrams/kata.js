1911fd97966363270563fdef8e69692200eb499b
dislersd <dislersd@gmail.com>
2020-04-20 16:25:40 -0700

    group anagrams

--- group_anagrams/kata.js
+++ group_anagrams/kata.js
@@ -1,41 +1,13 @@
 function groupAnagrams (strings) {
-  let res = []
   let strMap = {}
   for (let str of strings) {
-    if (!strMap[str]) {
-      strMap[str] = [str]
-    } else if (strMap[[...str].sort().join('')]) {
-      strMap[str].push(str)
+    if (!strMap[[...str].sort().join('')]) {
+      strMap[[...str].sort().join('')] = [str]
+    } else {
+      strMap[[...str].sort().join('')].push(str)
     }
   }
-  strMap
+  return Object.values(strMap)
 }
 
-groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"])
-
-
-
-        //  v        
-// Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
-// Output:
-// [
-//   ["ate","eat","tea"],
-//   ["nat","tan"],
-//   ["bat"]
-// ]
-// grouped by simialar characters
-// if all characters are the same in two words they go together
-// for each string -> for each char -> compare to each char of every other string
-// if 
-
-
-
-let x = {
-  eat: ['ate','tea'],
-  tea: [],
-  tan: [],
-  ate: [],
-  nat: [],
-  bat: []
-}
-
+console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"])) // => [ [ 'eat', 'tea', 'ate' ], [ 'tan', 'nat' ], [ 'bat' ] ]
