464c55ee2a0d9e01d1584b3e6f8d9ca7062a5cf4
dislersd <dislersd@gmail.com>
2020-05-03 21:13:34 -0700

    ransom note

--- ransom_note/kata.js
+++ ransom_note/kata.js
@@ -1,3 +1,21 @@
 let canConstruct = (ransomNote, magazine) => {
-  
-}
+  let magMap = new Map();
+  for (char of magazine) {
+    if (magMap.has(char)) {
+      magMap.set(char, magMap.get(char) + 1);
+    } else {
+      magMap.set(char, 1);
+    }
+  }
+
+  for (char of ransomNote) {
+    if (!magMap.get(char)) {
+      return false;
+    } else {
+      magMap.set(char, magMap.get(char) - 1);
+    }
+  }
+  return true;
+};
+
+console.log(canConstruct("cbb", "bbbc")); // => true
