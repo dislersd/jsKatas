5b84521e1bf083f57e6545d2aab69b58d38d49bf
dislersd <dislersd@gmail.com>
2020-05-18 18:57:04 -0700

    climbing stairs

---
+++ climbing_stairs/kata.js
@@ -0,0 +1,20 @@
+var climbStairs = function (n) {
+  if (n === 0 || n === 1) return 1;
+
+  let a = 1,
+    b = 2,
+    c,
+    count = 2;
+
+  while (count < n) {
+    c = a + b;
+    a = b;
+    b = c;
+    count++;
+  }
+
+  return b;
+};
+
+
+console.log(climbStairs(3))// => 3
